9ba4e556dac9af577d7544df52aeea3c9510c842
John Nguyen <johnv.nguyen14@gmail.com>
2024-04-19 15:56:54 -0400

    Finished Part 2: Expanding Functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
-// Part 3: Feeling Loop
+// Part 3: Feeling Loop (Refactored)
 
 let csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
@@ -95,15 +95,79 @@ cell5 = cell5.join("");
 cell5 = cell5.split(/[,\n]+/);
 console.log(cell5);
 
+///////////////////////////////////////////////////////////////////////////////////////////
 // Popping last empty element and Printing out arrays
 
-cell1.pop();
+cell1.pop(); // Gets rid of last empty element for cells 1 - 4
 cell2.pop();
 cell3.pop();
 cell4.pop();
 
-console.log(`${cell1}\n
+console.log(`Original output with 5 different cells: \n${cell1}\n
 ${cell2}\n
 ${cell3}\n
 ${cell4}\n
 ${cell5}\n`)
+
+console.log(`//////////////////////////////////////////////`)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Part 2: Expanding Functionality 
+
+let col = 0; // Variable to hold the number of columns in each row
+
+let csv2 = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+
+let row = 1;
+let row1 = [];
+let row2 = [];
+let row3 = [];
+let row4 = [];
+let row5 = [];
+
+
+
+for(let i = 0; i < csv2.length; i++){
+    let data = ""; // String for each row's data
+
+    if(csv2[i] === "\n"){ // If car is '\n' iterate row and continue
+        row++;
+        continue;
+    }
+
+    data += csv2[i]; // Updating data with per char in csv2
+
+    switch(row){ // Populate data into each row
+        case 1:
+            row1 += data;
+            break;
+        case 2:
+            row2 += data;
+            break;
+        case 3:
+            row3 += data;
+            break;
+        case 4:
+            row4 += data;
+            break;
+        case 5:
+            row5 += data;
+            break;
+    }
+}
+
+row1 = row1.split(/[,\n]+/); // Split all rows into arrays 
+row2 = row2.split(/[,\n]+/);
+row3 = row3.split(/[,\n]+/);
+row4 = row4.split(/[,\n]+/);
+row5 = row5.split(/[,\n]+/);
+
+console.log(row1)
+console.log(row2)
+console.log(row3)
+console.log(row4)
+console.log(row5)
+
+let row2D = [row1, row2, row3, row4, row5];
+
+console.log(row2D);
